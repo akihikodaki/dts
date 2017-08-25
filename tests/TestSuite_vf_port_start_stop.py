aad04feee4cc8f0721f17460ebaf188b9a0df70a
Fangfang Wei <fangfangx.wei@intel.com>
2017-08-25 16:54:40 +0800

    tests/vf_port_start_stop: remove crc-strip option
    Remove "crc-strip" option in vf_port_start_stop suite.
    Because of crc-strip default value is enabled in dpdk17.08.
    
    Signed-off-by: Fangfang Wei <fangfangx.wei@intel.com>

--- tests/TestSuite_vf_port_start_stop.py
+++ tests/TestSuite_vf_port_start_stop.py
@@ -214,7 +214,7 @@ class TestVfPortStartStop(TestCase):
         self.vm0_dut_ports = self.vm_dut_0.get_ports('any')
 
         self.vm0_testpmd = PmdOutput(self.vm_dut_0)
-        self.vm0_testpmd.start_testpmd(VM_CORES_MASK, '--crc-strip')
+        self.vm0_testpmd.start_testpmd(VM_CORES_MASK)
         self.vm0_testpmd.execute_cmd('set fwd mac')
 
         time.sleep(2)
