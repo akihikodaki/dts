caviumnetworks/dpdk-dts

commit 4792d001d5b51bdad66097e0fc8dd682fd863cd0

framework: Add login password support
DTS login dut and tester machine via ssh service while
public and private keys are needed.

That means it can't use the user:password to login, which
should not always be a good idea.

Add login with password for another choice.

Signed-off-by: Michael Qiu <michael.qiu@intel.com>

framework/etgen.py
@@ -137,7 +137,9 @@ class IxiaPacketGenerator(SSHConnection):
         self.tester = tester
         self.NAME = 'ixia'
         self.logger = getLogger(self.NAME)
-        super(IxiaPacketGenerator, self).__init__(self.get_ip_address(), self.NAME)
+        super(IxiaPacketGenerator, self).__init__(self.get_ip_address(),
+                                                  self.NAME,
+                                                  self.get_password())
         super(IxiaPacketGenerator, self).init_log(self.logger)
 
         self.tcl_cmds = []
@@ -169,6 +171,9 @@ class IxiaPacketGenerator(SSHConnection):
 
     def get_ip_address(self):
         return self.tester.get_ip_address()
+
+    def get_password(self):
+        return self.tester.get_password()
 
     def add_tcl_cmd(self, cmd):
         """

framework/crbs.py
@@ -10,6 +10,7 @@ crbs = [
      'user': '',
      'pass': '',
      'tester IP': '',
+     'tester pass': '',
      IXIA: None,
      'memory channels': 4,
      'bypass core0': True},

framework/dut.py
@@ -59,9 +59,11 @@ class Dut(Crb):
         super(Dut, self).__init__(crb, serializer)
         self.NAME = 'dut'
         self.logger = getLogger(self.NAME)
-        self.session = SSHConnection(self.get_ip_address(), self.NAME)
+        self.session = SSHConnection(self.get_ip_address(), self.NAME,
+                                     self.get_password())
         self.session.init_log(self.logger)
-        self.alt_session = SSHConnection(self.get_ip_address(), self.NAME)
+        self.alt_session = SSHConnection(self.get_ip_address(), self.NAME,
+                                         self.get_password())
         self.alt_session.init_log(self.logger)
         self.number_of_cores = 0
         self.tester = None
@@ -119,6 +121,12 @@ class Dut(Crb):
         Get DUT's ip address.
         """
         return self.crb['IP']
+
+    def get_password(self):
+        """
+        Get DUT's login password.
+        """
+        return self.crb['pass']
 
     def dut_prerequisites(self):
         """

framework/ssh_connection.py
@@ -40,8 +40,8 @@ class SSHConnection(object):
     Implement send_expect/copy function upper SSHPexpet module.
     """
 
-    def __init__(self, host, session_name):
-        self.session = SSHPexpect(host, USERNAME)
+    def __init__(self, host, session_name, password = ''):
+        self.session = SSHPexpect(host, USERNAME, password)
         self.name = session_name
 
     def init_log(self, logger):

framework/ssh_pexpect.py
@@ -12,12 +12,14 @@ Aslo support transfer files to tester or DUT.
 
 class SSHPexpect(object):
 
-    def __init__(self, host, username):
+    def __init__(self, host, username, password):
         try:
             self.session = pxssh.pxssh()
             self.username = username
             self.host = host
-            self.session.login(self.host, self.username)
+            self.password = password
+            self.session.login(self.host, self.username,
+                               self.password, original_prompt='[$#>]')
             self.send_expect('stty -echo', '# ', timeout=2)
         except Exception:
             raise SSHConnectionException(host)
@@ -69,21 +71,20 @@ class SSHPexpect(object):
         Copies a file from a remote place into local.
         """
         command = 'scp {0}@{1}:{2} .'.format(self.username, self.host, filename)
-        self._spawn_scp(command, password)
+        if password == '':
+            self._spawn_scp(command, self.password)
+        else:
+            self._spawn_scp(command, password)
 
     def copy_file_to(self, filename, password=''):
         """
         Sends a local file to a remote place.
         """
         command = 'scp {0} {1}@{2}:'.format(filename, self.username, self.host)
-        self._spawn_scp(command, password)
-
-    def copy_file_from(self, filename, password=''):
-        """
-        copy a remote file to a local place.
-        """
-        command = 'scp {1}@{2}:{0} .'.format(filename, self.username, self.host)
-        self._spawn_scp(command, password)
+        if password == '':
+            self._spawn_scp(command, self.password)
+        else:
+            self._spawn_scp(command, password)
 
     def _spawn_scp(self, scp_cmd, password):
         """

framework/tester.py
@@ -61,9 +61,11 @@ class Tester(Crb):
         self.NAME = 'tester'
 
         self.logger = getLogger(self.NAME)
-        self.session = SSHConnection(self.get_ip_address(), self.NAME)
+        self.session = SSHConnection(self.get_ip_address(),
+                                     self.NAME, self.get_password())
         self.session.init_log(self.logger)
-        self.alt_session = SSHConnection(self.get_ip_address(), self.NAME)
+        self.alt_session = SSHConnection(self.get_ip_address(),
+                                         self.NAME, self.get_password())
         self.alt_session.init_log(self.logger)
 
         self.ports_map = []
@@ -87,6 +89,12 @@ class Tester(Crb):
         Get ip address of tester CRB.
         """
         return self.crb['tester IP']
+
+    def get_password(self):
+        """
+        Get tester login password of tester CRB.
+        """
+        return self.crb['tester pass']
 
     def has_external_traffic_generator(self):
         """

akihikodaki/dts

commit 3fcb9e358fd6d913798665f03f0edd8a097609d5

framework/etgen: fix incorrect number of arguments
SSHConnection used in IxiaPacketGenerator takes 4 arguments i.e. host,
session_name, username and password.Currently only 3 arguments are being
passed while ssh object is being initilized in IxiaPacketGenerator.
Pass the missing username agrument by including self.tester.get_username().

Fixes: 4792d001d5b5 ("framework: Add login password support")

Signed-off-by: Jogarao Nartu <njogarao@caviumnetworks.com>

framework/etgen.py
@@ -140,7 +140,7 @@ class IxiaPacketGenerator(SSHConnection):
         self.NAME = 'ixia'
         self.logger = getLogger(self.NAME)
         super(IxiaPacketGenerator, self).__init__(self.get_ip_address(),
-                                                  self.NAME,
+                                                  self.NAME, self.tester.get_username(),
                                                   self.get_password())
         super(IxiaPacketGenerator, self).init_log(self.logger)
 