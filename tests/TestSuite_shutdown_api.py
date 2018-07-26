1bd34a69d7231155cb3a3690ead70cefb70e4d60
wenjieli <wenjiex.a.li@intel.com>
2018-07-26 16:58:58 +0800

    tests/shutdown_api: fix jumbo case to support sageville and sagepond
    vlan segment are not calculated in statistics with sageville and sagepond
    add filter to align enable_disablejumbo with hardware limitation
    
    Signed-off-by: wenjieli <wenjiex.a.li@intel.com>

--- tests/TestSuite_shutdown_api.py
+++ tests/TestSuite_shutdown_api.py
@@ -346,7 +346,7 @@ class TestShutdownApi(TestCase):
             self.dut.send_expect("port start all", "testpmd> ", 100)
             self.dut.send_expect("start", "testpmd> ")
 
-            if self.nic in ['magnolia_park', 'niantic', 'twinpond', 'kawela_4', 'ironpond', 'springfountain', 'springville', 'powerville']:
+            if self.nic in ['magnolia_park', 'niantic', 'twinpond', 'kawela_4', 'ironpond', 'springfountain', 'springville', 'powerville', 'sageville', 'sagepond']:
                 # nantic vlan length will not be calculated
                 vlan_jumbo_size = jumbo_size + 4
             else:
