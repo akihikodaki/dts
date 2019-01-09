704d921edd9d0db363be7daf929cece952d57dce
xueqin.lin <xueqin.lin@intel.com>
2019-01-08 21:25:31 -0500

    tests/unit_tests_lpm: add lpm perf and lpm6 perf script
    Signed-off-by: Xueqin Lin <xueqin.lin@intel.com>

--- tests/TestSuite_unit_tests_lpm.py
+++ tests/TestSuite_unit_tests_lpm.py
@@ -71,9 +71,11 @@ class TestUnitTestsLpmIpv6(TestCase):
 
     def test_lpm(self):
         """
-        Run lpm for IPv6 autotest.
+        Run lpm for IPv4 autotest.
         """
-        self.dut.send_expect("./%s/app/test -n 1 -c %s" % (self.target, self.coremask), "R.*T.*E.*>.*>", 60)
+        self.dut.send_expect(
+            "./%s/app/test -n 1 -c %s"
+            % (self.target, self.coremask), "R.*T.*E.*>.*>", 60)
         out = self.dut.send_expect("lpm_autotest", "RTE>>", 120)
         self.dut.send_expect("quit", "# ")
         self.verify("Test OK" in out, "Test failed")
@@ -82,8 +84,32 @@ class TestUnitTestsLpmIpv6(TestCase):
         """
         Run lpm for IPv6 autotest.
         """
-        self.dut.send_expect("./%s/app/test -n 1 -c %s" % (self.target, self.coremask), "R.*T.*E.*>.*>", 60)
+        self.dut.send_expect(
+            "./%s/app/test -n 1 -c %s"
+            % (self.target, self.coremask), "R.*T.*E.*>.*>", 60)
         out = self.dut.send_expect("lpm6_autotest", "RTE>>", 120)
+        self.dut.send_expect("quit", "# ")
+        self.verify("Test OK" in out, "Test failed")
+
+    def test_lpm_perf(self):
+        """
+        Run lpm for IPv4 performance autotest.
+        """
+        self.dut.send_expect(
+            "./%s/app/test -n 1 -c %s"
+            % (self.target, self.coremask), "R.*T.*E.*>.*>", 60)
+        out = self.dut.send_expect("lpm_perf_autotest", "RTE>>", 600)
+        self.dut.send_expect("quit", "# ")
+        self.verify("Test OK" in out, "Test failed")
+
+    def test_lpm_ipv6_perf(self):
+        """
+        Run lpm for IPv6 performance autotest.
+        """
+        self.dut.send_expect(
+            "./%s/app/test -n 1 -c %s"
+            % (self.target, self.coremask), "R.*T.*E.*>.*>", 60)
+        out = self.dut.send_expect("lpm6_perf_autotest", "RTE>>", 120)
         self.dut.send_expect("quit", "# ")
         self.verify("Test OK" in out, "Test failed")
 
