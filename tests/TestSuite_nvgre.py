3e0fcc6e3c356b4095e2695d3df0396c4e238489
qifu <qi.fu@intel.com>
2019-08-19 11:06:18 +0000

    tests/TestSuite_nvgre: add carlsville to support nic of nvgre
    Add carlsville to support nic of nvgre.
    Signed-off-by: qifu <qi.fu@intel.com>

--- tests/TestSuite_nvgre.py
+++ tests/TestSuite_nvgre.py
@@ -361,7 +361,7 @@ class TestNvgre(TestCase):
         nvgre Prerequisites
         """
         # this feature only enable in FVL now
-        if self.nic in ["fortville_eagle", "fortville_spirit", "fortville_spirit_single", "fortville_25g", "fortpark_TLV", "cavium_a063", "cavium_a064"]:
+        if self.nic in ["fortville_eagle", "fortville_spirit", "fortville_spirit_single", "fortville_25g", "fortpark_TLV", "cavium_a063", "cavium_a064", "carlsville"]:
             self.compile_switch = 'CONFIG_RTE_LIBRTE_I40E_INC_VECTOR'
         elif self.nic in ["sageville", "sagepond"]:
             self.compile_switch = 'CONFIG_RTE_IXGBE_INC_VECTOR'
