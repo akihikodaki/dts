508bd51217932b33c59b620b37757b954189a900
qifu <qi.fu@intel.com>
2019-08-19 11:06:20 +0000

    tests/TestSuite_ptype_mapping: add carlsville to support nic of ptype_mapping
    Add carlsville to support nic of ptype_mapping.
    Signed-off-by: qifu <qi.fu@intel.com>

--- tests/TestSuite_ptype_mapping.py
+++ tests/TestSuite_ptype_mapping.py
@@ -45,7 +45,7 @@ class TestPtype_Mapping(TestCase):
         Run at the start of each test suite.
         """
         self.verify(self.nic in ['fortville_eagle', 'fortville_spirit',
-            'fortville_spirit_single', 'fortville_25g', 'fortpark_TLV'],
+            'fortville_spirit_single', 'fortville_25g', 'fortpark_TLV', 'carlsville'],
             'ptype mapping test can not support %s nic' % self.nic)
         ports = self.dut.get_ports()
         self.verify(len(ports) >= 1, "Insufficient ports for testing")  
