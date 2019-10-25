74abdbc4c80b40b394b561e5e26f4d1f1a7c3b8b
zhang,yan <yanx.a.zhang@intel.com>
2019-10-25 16:39:22 +0800

    tests/TestSuite_kernelpf_iavf:Modify the wrong matching method
    Modify the wrong matching method.
    
    Signed-off-by: zhang,yan <yanx.a.zhang@intel.com>

--- tests/TestSuite_kernelpf_iavf.py
+++ tests/TestSuite_kernelpf_iavf.py
@@ -144,7 +144,7 @@ class TestKernelpfIavf(TestCase):
         self.sriov_vfs_port = self.dut.ports_info[
             self.used_dut_port]['vfs_port']
         out = self.dut.send_expect('ethtool %s' % self.host_intf, '#')
-        self.speed = re.findall('Supported link modes:   (\d*)', out)[0]
+        self.speed = re.findall('Speed: (\d*)', out)[0]
         if set_vf_mac is True:
             self.vf_mac = "00:01:23:45:67:89"
             self.dut.send_expect("ip link set %s vf 0 mac %s" %
