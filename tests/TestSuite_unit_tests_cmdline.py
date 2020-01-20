46381108ba4422b20146f05326adecc17dff6913
Haiyang Zhao <haiyangx.zhao@intel.com>
2020-01-20 16:39:05 +0800

    tests/unit_tests_cmdline: add create_eal_parameters func
    *.call create_eal_parameters before start test for ABI test.
    
    Signed-off-by: Haiyang Zhao <haiyangx.zhao@intel.com>

--- tests/TestSuite_unit_tests_cmdline.py
+++ tests/TestSuite_unit_tests_cmdline.py
@@ -58,8 +58,7 @@ class TestUnitTestsCmdline(TestCase):
         Run at the start of each test suite.
         """
         # icc compilation cost long long time.
-        cores = self.dut.get_core_list("all")
-        self.coremask = utils.create_mask(cores)
+        self.cores = self.dut.get_core_list("all")
 
     def set_up(self):
         """
@@ -71,8 +70,8 @@ class TestUnitTestsCmdline(TestCase):
         """
         Run cmdline autotests in RTE command line.
         """
-
-        self.dut.send_expect("./%s/app/test -n 1 -c %s" % (self.target, self.coremask), "R.*T.*E.*>.*>", 60)
+        eal_params = self.dut.create_eal_parameters(cores=self.cores)
+        self.dut.send_expect("./%s/app/test %s" % (self.target, eal_params), "R.*T.*E.*>.*>", 60)
         out = self.dut.send_expect("cmdline_autotest", "RTE>>", 60)
         self.dut.send_expect("quit", "# ")
         self.verify("Test OK" in out, "Test failed")
