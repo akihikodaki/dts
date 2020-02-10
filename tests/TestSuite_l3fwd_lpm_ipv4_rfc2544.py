4375cf6803b583c557646c3383c7dde9f478c3a4
yufengmx <yufengx.mo@intel.com>
2020-02-10 10:11:46 +0800

    tests/l3fwd_lpm_ipv4_rfc2544: save results to json file
    save results to json file.
    
    Signed-off-by: yufengmx <yufengx.mo@intel.com>

--- tests/TestSuite_l3fwd_lpm_ipv4_rfc2544.py
+++ tests/TestSuite_l3fwd_lpm_ipv4_rfc2544.py
@@ -66,7 +66,7 @@ class TestL3fwdLpmIpv4Rfc2544(TestCase, L3fwdBase):
         """
         Run after each test suite.
         """
-        pass
+        self.l3fwd_save_results(json_file="{}.json".format(self.suite_name))
 
     def set_up(self):
         """
