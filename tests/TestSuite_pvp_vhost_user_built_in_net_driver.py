ff4f3f05f44ca7c965d14aadfa24b9545e007f94
Xiao Qimai <qimaix.xiao@intel.com>
2020-02-26 17:37:18 +0800

    pvp_vhost_user_built_in_net_driver: remove unnecessary param --single-file-segments from vhost
    Signed-off-by: Xiao Qimai <qimaix.xiao@intel.com>

--- tests/TestSuite_pvp_vhost_user_built_in_net_driver.py
+++ tests/TestSuite_pvp_vhost_user_built_in_net_driver.py
@@ -172,8 +172,6 @@ class TestPVPVhostUserBuiltInNetDriver(TestCase):
         """
         self.dut.send_expect("rm -rf ./vhost.out", "#")
         eal_param = self.dut.create_eal_parameters(socket=self.ports_socket, cores=self.core_list_vhost_user, prefix='vhost')
-        if self.check_2M_env:
-            eal_param += " --single-file-segments"
         command_line_client = "./examples/vhost/build/app/vhost-switch " + eal_param + ' -- -p 0x1 --mergeable 0 --vm2vm 1 --builtin-net-driver --socket-file ./vhost-net> ./vhost.out &'
         self.vhost_switch.send_expect(command_line_client, "# ", 120)
         time.sleep(15)
