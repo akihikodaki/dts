230dc88b9a3c089e0f7dc4e4a2ecda93688b9c24
xizhan4x <xix.zhang@intel.com>
2020-09-04 16:18:34 +0800

    tests/TestSuite_pvp_multi_paths_virtio_single_core_performance.py:support meson build
    Signed-off-by: xizhan4x <xix.zhang@intel.com>

--- tests/TestSuite_pvp_multi_paths_virtio_single_core_performance.py
+++ tests/TestSuite_pvp_multi_paths_virtio_single_core_performance.py
@@ -72,6 +72,8 @@ class TestPVPMultiPathVirtioPerformance(TestCase):
         self.vhost = self.dut.new_session(suite="vhost")
         self.save_result_flag = True
         self.json_obj = {}
+        self.path=self.dut.apps_name['test-pmd']
+        self.testpmd_name = self.path.split("/")[-1]
 
     def set_up(self):
         """
@@ -159,12 +161,12 @@ class TestPVPMultiPathVirtioPerformance(TestCase):
         """
         self.dut.send_expect("rm -rf ./vhost.out", "#")
         self.dut.send_expect("rm -rf ./vhost-net*", "#")
-        self.dut.send_expect("killall -s INT testpmd", "#")
+        self.dut.send_expect("killall -s INT %s" % self.testpmd_name , "#")
         self.dut.send_expect("killall -s INT qemu-system-x86_64", "#")
         eal_param = self.dut.create_eal_parameters(socket=self.ports_socket, cores=self.core_list_host, prefix='vhost',
                                                    ports=[self.dut.ports_info[self.dut_ports[0]]['pci']],
                                                    vdevs=['net_vhost0,iface=vhost-net,queues=1,client=0'])
-        command_line_client = "./%s/app/testpmd " % self.target + eal_param + \
+        command_line_client = self.path + eal_param + \
                 ' -- -i --nb-cores=2 --txd=%d --rxd=%d' % (self.nb_desc, self.nb_desc)
         self.vhost.send_expect(command_line_client, "testpmd> ", 120)
         self.vhost.send_expect("set fwd io", "testpmd> ", 120)
@@ -177,7 +179,7 @@ class TestPVPMultiPathVirtioPerformance(TestCase):
         eal_param = self.dut.create_eal_parameters(socket=self.ports_socket, cores=self.core_list_user, prefix='virtio', no_pci=True, vdevs=['net_virtio_user0,mac=00:01:02:03:04:05,path=./vhost-net,%s' % args["version"]])
         if self.check_2M_env:
             eal_param += " --single-file-segments"
-        command_line_user = "./%s/app/testpmd " % self.target + eal_param + \
+        command_line_user = self.path + eal_param + \
             " -- -i %s --rss-ip --nb-cores=1 --txd=%d --rxd=%d" % (
                 args["path"], self.nb_desc, self.nb_desc)
         self.vhost_user.send_expect(command_line_user, "testpmd> ", 120)
