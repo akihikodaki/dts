ccde1260c4caa4cc68c7df42d599a06c858437e9
JiangYu <yux.jiang@intel.com>
2020-09-17 17:36:56 +0800

    tests/TestSuite_vhost_1024_ethports: support meson build
    1, adapt meson build: include app_name, killall app_name, build method
    2, optimize checkpoints.
    
    Signed-off-by: JiangYu <yux.jiang@intel.com>
    Tested-by: Jiang, YuX <yux.jiang@intel.com>
    Acked-by: lihongx Ma <lihongx.ma@intel.com>

--- tests/TestSuite_vhost_1024_ethports.py
+++ tests/TestSuite_vhost_1024_ethports.py
@@ -52,24 +52,30 @@ class TestVhost1024Ethports(TestCase):
         cores = self.dut.get_core_list("1S/2C/1T")
         self.pci_info = self.dut.ports_info[0]['pci']
         self.build_user_dpdk()
+        self.testpmd_path=self.dut.apps_name['test-pmd']
+        self.testpmd_name = self.testpmd_path.split("/")[-1]
 
     def set_up(self):
         """
         Run before each test case.
         """
         self.dut.send_expect('rm -rf ./vhost-net*', '# ')
-        self.dut.send_expect('killall -s INT testpmd', '# ')
+        self.dut.send_expect('killall -s INT %s' % self.testpmd_name, '# ')
         self.vhost_user = self.dut.new_session(suite='vhost-user')
 
     def build_user_dpdk(self):
-        self.dut.send_expect(
-            "sed -i 's/CONFIG_RTE_MAX_ETHPORTS=32$/CONFIG_RTE_MAX_ETHPORTS=1024/' config/common_base", '#', 30)
-        self.dut.build_install_dpdk(self.target)
+        if self.dut.build_type == "meson":
+            self.dut.build_install_dpdk(self.target, extra_options="-Dmax_ethports=1024")
+        else:
+            self.dut.send_expect("sed -i 's/CONFIG_RTE_MAX_ETHPORTS=32$/CONFIG_RTE_MAX_ETHPORTS=1024/' config/common_base", '#', 30)
+            self.dut.build_install_dpdk(self.target)
 
     def restore_dpdk(self):
-        self.dut.send_expect(
-            "sed -i 's/CONFIG_RTE_MAX_ETHPORTS=1024$/CONFIG_RTE_MAX_ETHPORTS=32/' config/common_base", '#', 30)
-        self.dut.build_install_dpdk(self.target)
+        if self.dut.build_type == "meson":
+            self.dut.build_install_dpdk(self.target, extra_options="-Dmax_ethports=32")
+        else:
+            self.dut.send_expect("sed -i 's/CONFIG_RTE_MAX_ETHPORTS=1024$/CONFIG_RTE_MAX_ETHPORTS=32/' config/common_base", '#', 30)
+            self.dut.build_install_dpdk(self.target)
 
     @property
     def check_2M_env(self):
@@ -88,9 +94,10 @@ class TestVhost1024Ethports(TestCase):
         for ethport in range(self.max_ethport):
             command_line_vdev += '--vdev "eth_vhost%d,iface=vhost-net%d,queues=%d" ' %(ethport, ethport, self.queue)
         eal_params = self.dut.create_eal_parameters(cores="1S/2C/1T", prefix='vhost', ports=[self.pci_info])
-        command_line_client = self.dut.target + '/app/testpmd ' + eal_params + command_line_vdev + ' -- -i'
+        command_line_client = self.testpmd_path + eal_params + command_line_vdev + ' -- -i'
         try:
             out = self.vhost_user.send_expect(command_line_client, 'testpmd> ', 120)
+            self.verify("Failed to create eth_vhost" not in out, "Failed to create some vhost_ethports")
         except Exception as e:
             self.verify(0, 'start testpmd failed')
         self.vhost_user.send_expect("quit", "#", 120)
