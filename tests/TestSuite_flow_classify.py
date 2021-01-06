b9364326c240bde83786997cb2c567f2b9dc95b1
Xie wei <weix.xie@intel.com>
2021-01-06 13:41:23 +0800

    tests/TestSuite_flow_classify:add cvl nic supported
    add cvl nic supported.
    
    Signed-off-by: Xie wei <weix.xie@intel.com>

--- tests/TestSuite_flow_classify.py
+++ tests/TestSuite_flow_classify.py
@@ -453,7 +453,7 @@ class TestFlowClassify(TestCase):
         self.verify_traffic(stm_types=stream_list, flow_type="multi_stream")
 
     def verify_supported_nic(self):
-        supported_drivers = ['i40e', 'ixgbe', 'igc', 'igb']
+        supported_drivers = ['i40e', 'ixgbe', 'igc', 'igb', 'ice']
         result = all([self.dut.ports_info[index]['port'].default_driver in
                       supported_drivers
                       for index in self.dut_ports])
