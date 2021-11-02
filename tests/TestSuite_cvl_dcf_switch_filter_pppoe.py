906549b1ac2028e0d5f40a1dde2fdcd7241ec8b1
Jiale Song <songx.jiale@intel.com>
2021-11-02 16:11:55 +0800

    tests/cvl_dcf_switch_filter_pppoe: update case code for driver change
    Signed-off-by: Jiale Song <songx.jiale@intel.com>

--- tests/TestSuite_cvl_dcf_switch_filter_pppoe.py
+++ tests/TestSuite_cvl_dcf_switch_filter_pppoe.py
@@ -527,6 +527,10 @@ class CVLDCFSwitchFilterPPPOETest(TestCase):
         #bind pf to kernel
         self.bind_nics_driver(self.dut_ports, driver="ice")
 
+        # get priv-flags default stats
+        self.flag = 'vf-vlan-pruning'
+        self.default_stats = self.dut.get_priv_flags_state(self.pf0_intf, self.flag)
+
         #set vf driver
         self.vf_driver = 'vfio-pci'
         self.dut.send_expect('modprobe vfio-pci', '#')
@@ -538,7 +542,8 @@ class CVLDCFSwitchFilterPPPOETest(TestCase):
         #get PF interface name
         self.pf0_intf = self.dut.ports_info[self.used_dut_port_0]['intf']
         out = self.dut.send_expect('ethtool -i %s' % self.pf0_intf, '#')
-        self.dut.send_expect('ethtool --set-priv-flags {} vf-vlan-prune-disable on'.format(self.pf0_intf), '#')
+        if self.default_stats:
+            self.dut.send_expect('ethtool --set-priv-flags %s %s off' % (self.pf0_intf, self.flag), "# ")
         #generate 4 VFs on PF
         self.dut.generate_sriov_vfs_by_port(self.used_dut_port_0, 4, driver=driver)
         self.sriov_vfs_port_0 = self.dut.ports_info[self.used_dut_port_0]['vfs_port']
@@ -914,5 +919,6 @@ class CVLDCFSwitchFilterPPPOETest(TestCase):
         """
         Run after each test suite.
         """
-        self.dut.send_expect('ethtool --set-priv-flags {} vf-vlan-prune-disable off'.format(self.pf0_intf), '#')
         self.dut.kill_all()
+        if self.default_stats:
+            self.dut.send_expect('ethtool --set-priv-flags %s %s %s' % (self.pf0_intf, self.flag, self.default_stats), "# ")
