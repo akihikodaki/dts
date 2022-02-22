bb6031f3e1c27def29ac4e8f5d316ee26f8962e1
Lingli Chen <linglix.chen@intel.com>
2022-02-22 13:17:54 +0800

    tests/unit_tests_mempool: mempool_perf_autotest increase timeout
    According to dpdk commit ed579e50c66d("mempool: test performance with constant n") A few more test cases were added.
    Increased the timeout value in memory pool performance autotest
    
    Signed-off-by: Lingli Chen <linglix.chen@intel.com>

--- tests/TestSuite_unit_tests_mempool.py
+++ tests/TestSuite_unit_tests_mempool.py
@@ -84,7 +84,7 @@ class TestUnitTestsMempool(TestCase):
         eal_params = self.dut.create_eal_parameters(cores=self.cores)
         app_name = self.dut.apps_name['test']
         self.dut.send_expect(app_name + eal_params,"R.*T.*E.*>.*>", 60)        
-        out = self.dut.send_expect("mempool_perf_autotest", "RTE>>", 2000)
+        out = self.dut.send_expect("mempool_perf_autotest", "RTE>>", 4500)
         self.dut.send_expect("quit", "# ")
         self.verify("Test OK" in out, "Test failed")
 
