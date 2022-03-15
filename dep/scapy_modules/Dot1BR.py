akihikodaki/dts

commit 5bdc6eee4cc707db46cfef1526e5098d1468b9ff

framework/*: Remove Dot1BR related code
align with dpdk cf47acc0f(ethdev: remove L2 tunnel offload control API)

Signed-off-by: Jun Dong <junx.dong@intel.com>
Reviewed-by: Lijuan Tu <lijuan.tu@intel.com>

framework/tester.py
@@ -113,10 +113,6 @@ class Tester(Crb):
         scapy_module_path = session.session.send_expect("os.path.dirname(sys.modules['scapy'].__file__)", '>>>')
         contrib_module_path = scapy_module_path.replace("'", "") + '/contrib'
 
-        # copy scapy module to tester
-        scapy_dep_module_path = lib_path + 'Dot1BR.py'
-        session.copy_file_to(src=scapy_dep_module_path, dst=contrib_module_path)
-
         # import scapy moudle to scapy APP
         out = session.session.send_expect(get_scapy_module_impcmd(), '>>> ')
         if 'ImportError' in out:

framework/packet.py
@@ -56,7 +56,6 @@ from scapy.packet import Raw
 from scapy.sendrecv import sendp
 from scapy.utils import hexstr, rdpcap, wrpcap
 
-from dep.scapy_modules.Dot1BR import Dot1BR
 
 from .utils import convert_int2ip, convert_ip2int, get_module_path
 
@@ -69,13 +68,8 @@ TMP_PATH = DTS_PATH[:-1] + '/output/tmp/pcap/' if exec_file.endswith('.pyc') els
 if not os.path.exists(TMP_PATH):
     os.system('mkdir -p %s' % TMP_PATH)
 
-# copy dep/Dot1BR to scapy
-_contrib_module_path = get_module_path('scapy.contrib')
-shutil.copy(DTS_PATH + '/dep/scapy_modules/Dot1BR.py', _contrib_module_path)
-
 scapy_modules_required = {'scapy.contrib.gtp': ['GTP_U_Header', 'GTPPDUSessionContainer'],
                           'scapy.contrib.lldp': ['LLDPDU', 'LLDPDUManagementAddress'],
-                          'scapy.contrib.Dot1BR': ['Dot1BR'],
                           'scapy.contrib.pfcp': ['PFCP'],
                           'scapy.contrib.nsh': ['NSH'],
                           'scapy.contrib.igmp': ['IGMP'],
@@ -101,7 +95,7 @@ def get_scapy_module_impcmd():
 PACKETGEN = "scapy"
 
 LayersTypes = {
-    "L2": ['ether', 'vlan', 'etag', '1588', 'arp', 'lldp', 'mpls', 'nsh'],
+    "L2": ['ether', 'vlan', '1588', 'arp', 'lldp', 'mpls', 'nsh'],
     # ipv4_ext_unknown, ipv6_ext_unknown
     "L3": ['ipv4', 'ipv4ihl', 'ipv6', 'ipv4_ext', 'ipv6_ext', 'ipv6_ext2', 'ipv6_frag'],
     "L4": ['tcp', 'udp', 'frag', 'sctp', 'icmp', 'nofrag'],
@@ -142,7 +136,6 @@ class scapy(object):
     SCAPY_LAYERS = {
         'ether': Ether(dst="ff:ff:ff:ff:ff:ff"),
         'vlan': Dot1Q(),
-        'etag': Dot1BR(),
         '1588': Ether(type=0x88f7),
         'arp': ARP(),
         'ipv4': IP(),
@@ -232,24 +225,6 @@ class scapy(object):
 
         if element == 'vlan':
             value = int(str(self.pkts[p_index][Dot1Q].vlan))
-        return value
-
-    def etag(self, pkt_layer, ECIDbase=0, prio=0, type=None):
-        if pkt_layer.name != "802.1BR":
-            return
-        pkt_layer.ECIDbase = int(ECIDbase)
-        pkt_layer.prio = prio
-        if type is not None:
-            pkt_layer.type = type
-
-    def strip_etag(self, element, p_index=0):
-        value = None
-
-        if self.pkts[p_index].haslayer('Dot1BR') == 0:
-            return None
-
-        if element == 'ECIDbase':
-            value = int(str(self.pkts[p_index][Dot1BR].ECIDbase))
         return value
 
     def strip_layer2(self, element, p_index=0):
@@ -416,7 +391,6 @@ class Packet(object):
         'TCP': {'layers': ['ether', 'ipv4', 'tcp', 'raw'], 'cfgload': True},
         'UDP': {'layers': ['ether', 'ipv4', 'udp', 'raw'], 'cfgload': True},
         'VLAN_UDP': {'layers': ['ether', 'vlan', 'ipv4', 'udp', 'raw'], 'cfgload': True},
-        'ETAG_UDP': {'layers': ['ether', 'etag', 'ipv4', 'udp', 'raw'], 'cfgload': True},
         'SCTP': {'layers': ['ether', 'ipv4', 'sctp', 'raw'], 'cfgload': True},
         'IPv6_TCP': {'layers': ['ether', 'ipv6', 'tcp', 'raw'], 'cfgload': True},
         'IPv6_UDP': {'layers': ['ether', 'ipv6', 'udp', 'raw'], 'cfgload': True},
@@ -812,7 +786,6 @@ class Packet(object):
         name2type = {
             'MAC': 'ether',
             'VLAN': 'vlan',
-            'ETAG': 'etag',
             'IP': 'ipv4',
             'IPv4-TUNNEL': 'inner_ipv4',
             'IPihl': 'ipv4ihl',
@@ -935,9 +908,6 @@ class Packet(object):
 
     def strip_element_vlan(self, element, p_index=0):
         return self.pktgen.strip_vlan(element, p_index)
-
-    def strip_element_etag(self, element, p_index=0):
-        return self.pktgen.strip_etag(element, p_index)
 
     def strip_element_layer4(self, element, p_index=0):
         return self.pktgen.strip_layer4(element, p_index)

tests/TestSuite_generic_flow_api.py
@@ -407,7 +407,7 @@ class TestGeneric_flow_api(TestCase):
             cmd_fmt = "flow %(create)s 0 ingress pattern %(flow)s / end actions %(action)s end"
         # Record the elements of the rule, ready for the configuration
         # of packets inconsistent to the rule.
-        extrapacket = {'vlan': '', 'etag': '', 'ipv4': '', 'ipv6': '', 'sip': '', 'dip': '', 'proto': '', 'tos': '',
+        extrapacket = {'vlan': '', 'for ': '', 'ipv4': '', 'ipv6': '', 'sip': '', 'dip': '', 'proto': '', 'tos': '',
                        'ttl': '', 'tcp': '', 'udp': '', 'sctp': '', 'sport': '', 'dport': '', 'vni': '', 'tni': '',
                        'ineth': '', 'invlan': ''}
         # Define the packet string, which is consistent to the flow rule.
@@ -420,12 +420,6 @@ class TestGeneric_flow_api(TestCase):
         if 'fuzzy' in flows:
             thresh = self.generate_random_int(1, 15)
             flow_str = "fuzzy thresh is %d " % thresh
-        # L2 tunnel
-        elif 'etag' in flows:
-            ecid = self.generate_random_int(4096, 8191)
-            flow_str = "e_tag grp_ecid_b is %s " % hex(ecid)
-            pkt += "/Dot1BR(GRP=0x1, ECIDbase=%s)" % hex(ecid - 4096)
-            extrapacket['etag'] = hex(ecid - 4096)
         else:
             flow_str = "eth "
         # Configure the flow and packet and extrapacket string

commit 772dd31c6f1e1b1b14649ff0029afb7392ccf702

remove dep/scapy_modules
As dot1br removed, the scapy_modules is retired.

Fixes: 5bdc6eee4cc7 ("framework/*: Remove Dot1BR related code")

Signed-off-by: Lijuan Tu <lijuan.tu@intel.com>

dep/scapy_modules/Dot1BR.py
@@ -1,34 +1,0 @@
-## This file is part of Scapy
-## See http://www.secdev.org/projects/scapy for more informations
-## Copyright (C) Philippe Biondi <phil@secdev.org>
-## This program is published under a GPLv2 license
-
-## Copyright (c) 2016 Marvin liu <yong.liu@intel.com>
-
-"""
-VBPE (virtual brige port extenstion)
-"""
-
-from scapy.data import ETHER_TYPES
-from scapy.fields import *
-from scapy.layers.l2 import Ether
-from scapy.packet import *
-
-
-class Dot1BR(Packet):
-    name = "802.1BR"
-    aliastypes = [ Ether ]
-    fields_desc =  [
-                     BitField("EPCP", 0, 3),
-                     BitField("EEDI", 0, 1),
-                     BitField("IngressECIDbase", 0, 12),
-                     BitField("Reserverd", 0, 2),
-                     BitField("GRP", 0, 2),
-                     BitField("ECIDbase", 0, 12),
-                     BitField("IngressECIDext", 0, 8),
-                     BitField("ECIDext", 0, 8),
-                     XShortEnumField("type", 0x0000, ETHER_TYPES) ]
-    def mysummary(self):
-        return self.sprintf("802.1BR E-CID %Ingress_E-CID_base%")
-
-bind_layers(Ether, Dot1BR, type=0x893F)

dep/scapy_modules/__init__.py
@@ -1,30 +1,0 @@
-#!/usr/bin/python3
-# BSD LICENSE
-#
-# Copyright (c) 2021 PANTHEON.tech s.r.o.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#   * Redistributions of source code must retain the above copyright
-#     notice, this list of conditions and the following disclaimer.
-#   * Redistributions in binary form must reproduce the above copyright
-#     notice, this list of conditions and the following disclaimer in
-#     the documentation and/or other materials provided with the
-#     distribution.
-#   * Neither the name of PANTHEON.tech s.r.o. nor the names of its
-#     contributors may be used to endorse or promote products derived
-#     from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.

framework/settings.py
@@ -43,7 +43,6 @@ FOLDERS = {
     'Depends': 'dep',
     'Output': 'output',
     'NicDriver': 'nics',
-    'scapy_modules': 'scapy_modules'
 }
 
 """

framework/tester.py
@@ -106,18 +106,6 @@ class Tester(Crb):
         session = self.create_session(session_name)
         self.scapy_sessions_li.append(session)
         session.send_expect('scapy', '>>> ')
-        file_dir = os.path.dirname(__file__).split(os.path.sep)
-        lib_path = os.path.sep.join(file_dir[:-1]) + '/dep/scapy_modules/'
-
-        # get contrib module path on tester
-        scapy_module_path = session.session.send_expect("os.path.dirname(sys.modules['scapy'].__file__)", '>>>')
-        contrib_module_path = scapy_module_path.replace("'", "") + '/contrib'
-
-        # import scapy moudle to scapy APP
-        out = session.session.send_expect(get_scapy_module_impcmd(), '>>> ')
-        if 'ImportError' in out:
-            session.logger.warning(f'entering import error: {out}')
-
         return session
 
     def check_scapy_version(self):

tests/TestSuite_vxlan_gpe_support_in_i40e.py
@@ -122,11 +122,6 @@ class VxlanGpeTestConfig(object):
         """
         Send vxlan pcap file by iface
         """
-        # load vxlan module to scapy
-        cwd = os.getcwd()
-        dir_vxlan_module = cwd + r'/' + FOLDERS['Depends'] + r'/' + FOLDERS['scapy_modules']
-        self.test_case.tester.scapy_append("sys.path.append('%s')" % dir_vxlan_module)
-        self.test_case.tester.scapy_append("from scapy.layers.vxlan import VXLAN")
         self.test_case.tester.scapy_append(
             'pcap = rdpcap("%s")' % self.pcap_file)
         self.test_case.tester.scapy_append(